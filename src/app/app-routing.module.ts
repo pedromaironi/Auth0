e1941a6ab259bd87efbdbf928fab045c2e032471
Pedro Toribio <pedrom_t@hotmail.com>
2020-04-14 13:12:33 -0400

    Auth-1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { CallbackComponent } from './components/callback/callback.component';
+import { AuthGuard } from './services/auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'precios', component: PreciosComponent },
+  { path: 'protegida', component: ProtegidaComponent,
+  canActivate: [AuthGuard] },
+  { path: 'callback', component: CallbackComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
---
+++ src/app/components/protegida/protegida.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-protegida',
+  templateUrl: './protegida.component.html',
+  styles: []
+})
+export class ProtegidaComponent implements OnInit {
+
+  constructor( public auth: AuthService) {
+
+   }
+
+  ngOnInit() {
+    this.auth.userProfile$.subscribe( perfil => {
+      console.log(perfil);
+    });
+  }
+
+}
